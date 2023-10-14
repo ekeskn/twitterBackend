fc963ae612c48ebc3b9afc1f03cabb65f27b5cad
ekeskn <ege-keskintas@hotmail.com>
2023-10-14 18:34:22 +0300

    user CRUD operations added

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -40,15 +40,30 @@ router.get("/:id", async (req, res) => {
 });
 
 //update user
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `not implemented ${id}` });
+  const { bio, name, image } = req.body;
+  try {
+    const result = await prisma.user.update({
+      where: { id: Number(id) },
+      data: {
+        bio,
+        name,
+        image,
+      },
+    });
+
+    res.json(result)
+  } catch (e) {
+    res.status(400).json({ error: "failed to update user" });
+  }
 });
 
 //delete user
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `not implemented ${id}` });
+  await prisma.user.delete({where:{id:Number(id)}});
+  res.sendStatus(200);
 });
 
 export default router;
