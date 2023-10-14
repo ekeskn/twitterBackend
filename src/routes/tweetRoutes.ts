c271627f0111c5654a73ac5da7be1bfc958cf069
ekeskn <ege-keskintas@hotmail.com>
2023-10-15 00:23:37 +0300

    CRUD operations for Tweet

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,36 +1,70 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 //tweet CRUD
 
 //create tweet
-router.post("/", (req, res) => {
-  res.status(501).json({ error: "not implemented" });
+router.post("/", async (req, res) => {
+  const { content, userId } = req.body;
+  try {
+    const result = await prisma.tweet.create({
+      data: {
+        content,
+        userId
+      },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ e });
+  }
 });
 
 //list tweet
-router.get("/", (req, res) => {
-  res.status(501).json({ error: "not implemented" });
+router.get("/", async (req, res) => {
+  const allTweet = await prisma.tweet.findMany();
+
+  res.json({ allTweet });
 });
 
 //get one tweet
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-
-  res.status(501).json({ error: `not implemented ${id}` });
+  const tweet = await prisma.tweet.findUnique({ where: { id: Number(id) } });
+  if(!tweet){
+   return res.status(404).json({error:"tweet not found"})
+  }
+  res.json({ tweet });
 });
 
 //update tweet
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `not implemented ${id}` });
+  const { content, image,impressions } = req.body;
+  try {
+    const result = await prisma.tweet.update({
+      where: { id: Number(id) },
+      data: {
+        content,
+        image,
+        impressions
+      },
+    });
+
+    res.json(result)
+  } catch (e) {
+    res.status(400).json({ error: "failed to update user" });
+  }
 });
 
 //delete tweet
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-  res.status(501).json({ error: `not implemented ${id}` });
+  await prisma.tweet.delete({where:{id:Number(id)}});
+  res.sendStatus(200);
 });
 
 export default router;
